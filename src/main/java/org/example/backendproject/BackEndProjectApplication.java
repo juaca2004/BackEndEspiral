614c62138fc6dad4dbb8eb68462bcb91accacbfc
juaca2004 <114953837+juaca2004@users.noreply.github.com>
2024-04-03 13:01:23 -0500

    first commit

---
+++ src/main/java/org/example/backendproject/BackEndProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/backendproject/BackEndProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
